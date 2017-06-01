b58835801dda6b89bea358a758be0f79d419d74e
Anna Kuznetsova and Saul Artiga <anna+saul@>
2017-06-01 14:24:26 -0700

    add js effects and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  
+  $("form#user").submit(function(event) {
+    event.preventDefault();
+    console.log("submitted");
+    var name = $("#name").val();
+    var age = $("#age").val();
+    var color = $("#color").val();
+
+    if (color === "red"){
+      var celebrity = "Leonardo DiCaprio";
+    } else if (color === "blue"){
+      var celebrity = "Skeletor";
+    } else if (color === "yellow"){
+      var celebrity = "Skeletor again";
+    } else {
+      $(".celebritydate").empty().append();
+    }
+    //$(".celebritydate").empty().append(celebrity);
+    $(".skeletor").toggle();
+  });
 });
